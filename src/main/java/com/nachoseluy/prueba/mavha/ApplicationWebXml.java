24fd7ebf27934787f81a46cb9076635e757a47e3
Nacho Seluy <nachoseluy@hotmail.com>
2018-12-26 21:04:56 -0300

    Initial application generated by JHipster-4.4.1

---
+++ src/main/java/com/nachoseluy/prueba/mavha/ApplicationWebXml.java
@@ -0,0 +1,21 @@
+package com.nachoseluy.prueba.mavha;
+
+import com.nachoseluy.prueba.mavha.config.DefaultProfileUtil;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * This is a helper Java class that provides an alternative to creating a web.xml.
+ * This will be invoked only when the application is deployed to a servlet container like Tomcat, JBoss etc.
+ */
+public class ApplicationWebXml extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        /**
+         * set a default to use when no profile is configured.
+         */
+        DefaultProfileUtil.addDefaultProfile(application.application());
+        return application.sources(PruebamavhaApp.class);
+    }
+}
---
+++ src/main/java/com/nachoseluy/prueba/mavha/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.nachoseluy.prueba.mavha.web.rest.vm;
